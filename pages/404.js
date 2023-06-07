f6a2fec50f2d096aa83d40814c9398c48d06d493
Crispin CIRHUZA <ccirhuza.ext@orange.com>
2023-06-07 16:19:18 +0100

    nightly

--- pages/404.js
+++ pages/404.js
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { Typography, useTheme } from "@mui/material";
-import { PubLayout } from "../src/components";
+import { PubLayout, Welcome } from "../src/components";
 
 const Page404 = ({}) => {
   const theme = useTheme();
